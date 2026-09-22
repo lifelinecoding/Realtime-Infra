99723be0ff23c0c1bb3a352fb713cab237682d3b
lifelinecoding <apatel89329@gmail.com>
2026-09-22 16:37:03 +0530

    feat: set up Socket.IO server with Express and CORS support
    - Added server.js to initialize an Express server with Socket.IO
    - Configured CORS to allow requests from http://localhost:5173
    - Updated package.json to include express, cors, and nodemon as dependencies
    - Added start script to run the server using nodemon

--- Sockets/client/src/App.jsx
+++ Sockets/client/src/App.jsx
@@ -1,5 +1,23 @@
 
+import { useEffect } from "react"
+import { io } from "socket.io-client"
+
 function App() {
+
+  useEffect(() => {
+    const socket = io("http://localhost:3000");
+    socket.on("connect" , () =>{
+      console.log("Connecting to server");
+    })
+
+    socket.on("message", (data) => {
+      console.log(data);
+    })
+
+    return () =>{
+      // socket.disconnect();
+    }
+  }, []);
 
   return (
     <>
---
+++ Sockets/server/server.js
@@ -0,0 +1,38 @@
+import express from "express";
+import { Server } from "socket.io";
+import { createServer } from "node:http";
+import cors from "cors";
+
+const app = express();
+
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  }),
+);
+
+const httpServer = createServer(app);
+const server = new Server(httpServer, {
+  cors: {
+    origin: "http://localhost:5173",
+    credentials: true,
+  },
+});
+
+const PORT = 3000;
+
+server.on("connection", (socket) => {
+  console.log("A User connected: ", socket.id);
+
+  socket.emit("message", "Kaise ho bhai");
+
+});
+
+app.route("/").get((req, res) => {
+  res.send("Hello World!");
+});
+
+httpServer.listen(PORT, () => {
+  console.log(`Server is running at: ${PORT}`);
+});
